55a224a0a2b9674e031f8cd6c4296d404a618c1a
asyrafsarkei <muhamadasyraf.sarkei@gmail.com>
2025-07-22 11:01:17 +0800

    Create vite.config.js

---
+++ client/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  }
+});
