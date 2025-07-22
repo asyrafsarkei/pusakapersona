0abe0eb4f00be14587cfdbc24c2954ed9ce47c35
asyrafsarkei <muhamadasyraf.sarkei@gmail.com>
2025-07-22 11:12:23 +0800

    Update vite.config.ts
    add line base: "/pusakapersona/",

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/pusakapersona/",
   server: {
     host: true, // This makes the server accessible externally
     port: 5173, // Ensure this is the port you want to use
