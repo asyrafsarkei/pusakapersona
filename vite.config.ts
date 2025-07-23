0a5737a0e1c5b97c6c7673826a08401915c00eaf
Asyraf <muhamadasyraf.sarkei@gmail.com>
2025-07-23 17:33:26 +0800

    New vite version

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'client',
   plugins: [react()],
   base: "/pusakapersona/",
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
   server: {
     host: true, // This makes the server accessible externally
     port: 5173, // Ensure this is the port you want to use
   }
-})
+});
